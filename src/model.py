c4dda36582859bae6a7cc39696cb4bbd976f1bc2
brianhuang1019 <brianhuang1019@gmail.com>
2017-11-16 23:23:43 +0800

    modify class parameters

--- src/model.py
+++ src/model.py
@@ -9,18 +9,24 @@ from torch.autograd import Variable
 
 class CNNSentanceClassifier(nn.Module):
   
-  def __init__(self, wv_dims, n_class, channel=100):
+  def __init__(self, args):
     super().__init__()
-    self.wv_dims = wv_dims
-    self.n_class = n_class
-    self.channel = channel
 
-    self.conv1 = nn.Conv1d(self.wv_dims, self.channel, 3)
-    self.conv2 = nn.Conv1d(self.wv_dims, self.channel, 4)
-    self.conv3 = nn.Conv1d(self.wv_dims, self.channel, 5)
+    # random seed
+    torch.manual_seed(args.seed)
+    if args.cuda:
+      torch.cuda.manual_seed(args.seed)
 
-    self.drop = nn.Dropout(p=0.5, inplace=True)
-    self.fc = nn.Linear(self.channel*3, self.n_class)
+    self.wv_dims = args.wv_dims
+    self.kernels = args.kernels
+    self.n_class = args.n_class
+
+    self.conv1 = nn.Conv1d(self.wv_dims, self.kernels, 3)
+    self.conv2 = nn.Conv1d(self.wv_dims, self.kernels, 4)
+    self.conv3 = nn.Conv1d(self.wv_dims, self.kernels, 5)
+
+    self.drop = nn.Dropout(p=args.dropout, inplace=True)
+    self.fc = nn.Linear(self.kernels*3, self.n_class)
 
   def forward(self, x):
     '''
@@ -39,19 +45,19 @@ class CNNSentanceClassifier(nn.Module):
                 N equals to batch size
                 n_cls is the class num we want to discriminate
     '''
-    x1 = self.conv1(x)  # shape=(N, self.channel, L')
-    x2 = self.conv2(x)  # shape=(N, self.channel, L'')
-    x3 = self.conv3(x)  # shape=(N, self.channel, L''')
+    x1 = self.conv1(x)  # shape=(N, self.kernels, L')
+    x2 = self.conv2(x)  # shape=(N, self.kernels, L'')
+    x3 = self.conv3(x)  # shape=(N, self.kernels, L''')
 
     m1 = nn.MaxPool1d(x1.size()[-1])  # kernel size = L'
     m2 = nn.MaxPool1d(x2.size()[-1])  # kernel size = L''
     m3 = nn.MaxPool1d(x3.size()[-1])  # kernel size = L'''
 
-    x1 = m1(x1).view(-1, self.channel)  # shape=(N, self.channel)
-    x2 = m2(x2).view(-1, self.channel)  # shape=(N, self.channel)
-    x3 = m3(x3).view(-1, self.channel)  # shape=(N, self.channel)
+    x1 = m1(x1).view(x.size()[0], self.kernels)  # shape=(N, self.kernels)
+    x2 = m2(x2).view(x.size()[0], self.kernels)  # shape=(N, self.kernels)
+    x3 = m3(x3).view(x.size()[0], self.kernels)  # shape=(N, self.kernels)
 
-    x_cat = torch.cat((x1, x2, x3), 1)  # shape=(N, self.channel*3)
+    x_cat = torch.cat((x1, x2, x3), 1)  # shape=(N, self.kernels*3)
     out   = self.fc(self.drop(x_cat))   # shape=(N, self.n_class)
 
     return out
@@ -59,10 +65,18 @@ class CNNSentanceClassifier(nn.Module):
 
 if __name__ == "__main__":
   # check output shape
-  batch, wv_dims, sent_len, n_class = 10, 100, 17, 2
-  model = CNNSentanceClassifier(wv_dims=wv_dims, n_class=n_class)
+  batch, wv_dims, sent_len, n_class = 10, 100, 17, 5
+  class Arg:
+    seed    = 1
+    cuda    = 0
+    wv_dims = wv_dims
+    kernels = 666
+    n_class = n_class
+    dropout = 0.5
+  model = CNNSentanceClassifier(Arg())
   input = torch.autograd.Variable(torch.randn(batch, wv_dims, sent_len))
   output = model(input)
   assert len(output.size()) == 2
   assert output.size()[0] == batch
   assert output.size()[1] == n_class
+  print("Pass Test")
