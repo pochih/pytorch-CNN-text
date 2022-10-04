7f4f6c6e40042576c478297cd1d6fa42fabe51ab
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-04 23:29:01 +0000

    Adding numeric_owner as keyword arguement

--- src/MR_loader.py
+++ src/MR_loader.py
@@ -160,7 +160,7 @@ class MovieReviewDataset(Dataset):
               if not is_within_directory(path, member_path):
                   raise Exception("Attempted Path Traversal in Tar File")
       
-          tar.extractall(path, members, numeric_owner) 
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
           
       
       safe_extract(tfile, self.data_dir)
--- src/polarity_loader.py
+++ src/polarity_loader.py
@@ -139,7 +139,7 @@ class PolarityDataset(Dataset):
               if not is_within_directory(path, member_path):
                   raise Exception("Attempted Path Traversal in Tar File")
       
-          tar.extractall(path, members, numeric_owner) 
+          tar.extractall(path, members, numeric_owner=numeric_owner) 
           
       
       safe_extract(tfile, self.data_dir)
